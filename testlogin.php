7b45a082177e1f7ae445b3b76b20432202555a0c
swanyriver <swanyriver@gmail.com>
2015-12-04 22:00:27 -0800

    testing harness is working

--- testlogin.php
+++ testlogin.php
@@ -1,19 +1,74 @@
- <!DOCTYPE html>
-  <html>
-  <head>
-    <link rel="stylesheet" href="css/final-style.css" />
-    <title>Login for Lender</title>
-  </head>
-  <body>
-  testing login
-  <br>
-  <iframe src="login.php"></iframe>
-  <br>
-  <iframe src="login.php"></iframe>
- 
+ <?php  
+
+$testcases = array();
+/*$testcases[n] = array(
+    'request' => , ""
+    'username' => , ""
+    'password' => , ""
+    'testname' => , ""
+    'expectedstate' => , ""
+    'expectedid' => , ""
+);*/
+$testcases[0] = array(
+    'request' =>  "login",
+    'username' => "Brandon",
+    'password' =>  "Garbage",
+    'testname' =>  "Bad Password",
+    'expectedstate' =>  "log in refused",
+    'expectedid' =>  "Not Set",
+);
+$testcases[1] = array(
+    'request' =>  "login",
+    'username' => "Brandon",
+    'testname' =>  "Password not supplied",
+    'expectedstate' =>  "log in refused",
+    'expectedid' =>  "Not Set",
+);
+$testcases[2] = array(
+    'request' =>  "signup",
+    'username' =>  "Brandon",
+    'password' =>  "doesntmatter",
+    'testname' =>  "User Name Already Taken",
+    'expectedstate' =>  "Sign up refused",
+    'expectedid' =>  "Not Set",
+);
+$testcases[3] = array(
+    'request' =>  "signup",
+    'username' =>  "newuser",
+    'password' =>  "",
+    'testname' =>  "Blank password provided",
+    'expectedstate' =>  "Sign up refused",
+    'expectedid' =>  "Not Set",
+);
+$testcases[4] = array(
+    'request' =>  "login",
+    'username' =>  "Brandon",
+    'password' =>  "password",
+    'testname' =>  "Succsessfull login",
+    'expectedstate' =>  "User is Logged in Succsessfully",
+    'expectedid' =>  "9",
+);
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/unittest.css" />
+</head>
+<body>
+
   <?php 
 
-  echo "<br><iframe src=\"login.php\"></iframe>";
+  #echo "<br><iframe src=\"testloginframe.php?request=login&username=Bob&password=Bob\"></iframe>";
+
+  foreach ($testcases as $case) {
+      echo "<iframe class=\"testframe\" src=\"testloginframe.php?";
+      foreach ($case as $key => $value) {
+          echo "$key=$value&";
+      }
+      echo "\"> </iframe>";
+  }
 
   ?>
 
---
+++ testloginframe.php
@@ -0,0 +1,27 @@
+<?php 
+
+foreach ($_GET as $key => $value) {
+    $_POST[$key] = $value;
+}
+
+echo "<h3 class=\"testname\"> TEST: {$_GET['testname']} </h3>";
+if (isset($_POST['request'])) echo "request:{$_POST['request']} <br> ";
+if (isset($_POST['username'])) echo "username:{$_POST['username']} <br> ";
+if (isset($_POST['password'])) echo "password:{$_POST['password']} <br> ";
+echo "<br>
+<h4 class=\"expectedstate\"> Expected State: {$_GET['expectedstate']} </h4>
+<br>
+";
+
+include 'login.php';
+
+
+
+echo "<br><br>user_id expected: {$_GET['expectedid']}   user_id:";
+if (!isset($_SESSION['user_id'])){
+    echo "not set <br>";
+} else {
+    echo "{$_SESSION['user_id']} <br>";
+}
+
+?>
