8cf7a4642265bd3f8251ea9b6327de85f53fb5f7
swanyriver <swanyriver@gmail.com>
2015-12-04 20:40:41 -0800

    frames allow php to run more than once

---
+++ testlogin.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+  <html>
+  <head>
+    <link rel="stylesheet" href="css/final-style.css" />
+    <title>Login for Lender</title>
+  </head>
+  <body>
+  testing login
+  <br>
+  <iframe src="login.php"></iframe>
+  <br>
+  <iframe src="login.php"></iframe>
+ 
+  <?php 
+
+  echo "<br><iframe src=\"login.php\"></iframe>";
+
+  ?>
+
+  </body>
+  </html>
+
